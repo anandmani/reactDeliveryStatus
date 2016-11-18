c5270a8a634e268d293ac04fc050b27ec5f0a1ad
Anand Mani <cma.camelot@gmail.com>
2016-11-18 13:49:13 +0530

    Formatted date fetched from API call

--- dev/js/components/App.js
+++ dev/js/components/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import {Component} from 'react';
 import TrackWidget from './TrackWidget';
+require('../../css/style.css');
 
 class App extends Component{
 
   constructor(){
     super();
     this.checkEnter = this.checkEnter.bind(this);
-    this.state = ({orderId : 3515342});
+    this.state = ({orderId : 1});
   }
 
   checkEnter(event){
--- dev/js/components/TrackWidget.js
+++ dev/js/components/TrackWidget.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Component} from 'react';
-
+var moment = require ('moment');
 //----
 // JSON format:
 // meta{}--status
@@ -44,14 +44,13 @@ class TrackWidget extends Component{
     this.key = "2e9b19ac-8e1f-41ac-a35b-4cd23f41ae17";
     this.orderJSON = {};
     this.fetchJSON = this.fetchJSON.bind(this);
+    this.state = {orderJSON: null};
   }
 
   fetchJSON(orderId){
-    return new Promise((resolve,reject)=>{
       console.log("Making Get Call to Fetch Order Status JSON");
       var getOrder = new XMLHttpRequest();
-      var url = `https://www.clickpost.in/api/v2/track-order/?username=${this.username}&key=${this.key}&waybill=${this.props.orderId}`;
-      var that = this;
+      var url = `https://www.clickpost.in/api/v2/track-order/?username=${this.username}&key=${this.key}&waybill=${orderId}`;
       getOrder.open("GET", url, true);
       getOrder.onreadystatechange = () =>
       {
@@ -59,67 +58,99 @@ class TrackWidget extends Component{
               //The above two statements can be replaced by getOrder.onload = () =>    have a look!
               if(getOrder.status === 200)
               {
-
-                // //Making a second call here
-                //   var secondCall = new XMLHttpRequest();
-                //   secondCall.open("GET",url,true);
-                //   secondCall.onreadystatechange = () => {
-                //     if(secondCall.readyState == 4){
-                //       if(secondCall.status == 200){
-                //         console.log("2 "+secondCall.responseText);
-                //       }
-                //     }
-                //   }
-                //   secondCall.send(null);
-
-                  var result = getOrder.responseText;
-                  this.orderJSON = result;
-                  console.log("1"+this.orderJSON);
-                  resolve(result);
+                   console.log(JSON.parse(getOrder.response));
+                   this.setState({orderJSON: JSON.parse(getOrder.response)});
               }
               else{
                 console.log("Something went wrong; Status: "+getOrder.status);
-                reject(Error(getOrder.status));
               }
           }
       }
       getOrder.send(null);
-
-    });
   }
 
+  componentWillReceiveProps(nextProps){
+    console.log("next props",nextProps);
+    if(nextProps!=this.props){//Best practice to compare them.
+      this.fetchJSON(nextProps.orderId);
+    }
+  }
+
+
   render(){
-    var res =0 ;
     console.log("Rendering widget, order id is "+this.props.orderId);
-    this.fetchJSON(this.props.orderId).then(
-      function(response){
-        res   = console.log("Returned from promise "+response)
-      },
-      function(error){
-        console.error("Failed!",error);
-      }
-    );
-    console.log("res;::"+res);
-    return(
-      <div id="orderStatus">
-        <div id="orderDetails">
+    if(this.state.orderJSON != null && parseInt(Object.keys(this.state.orderJSON["result"])[0])==this.props.orderId){  //The first part checks if JSON is not empty.  The second part checks if JSON for new orderId has been fetched, the condition fails if the JSON belongs to the previous orderId
+      console.log(this.state.orderJSON["result"][`${this.props.orderId}`]);
+      var scansArray = [...this.state.orderJSON["result"][`${this.props.orderId}`]["scans"]];
+      //Sorting Array
+      scansArray.sort((obj1,obj2)=>{
+        //moment.utc() to convert date string to moment obj
+        //obj1<obj2 return -1
+        //date1.diff(date) returns positive if date1 < date2
+        if(moment.utc(obj1.date).diff( moment.utc(obj2.date) ) <= 0){
+          return -1;
+        }
+        else{
+          return 1;
+        }
+      });
+      //Removing duplicates in the Array //Immediately invoked function
+      (function removeDuplicates(){
+        console.log("removing duplicates");
+        for(var i = 0; i< scansArray.length-1; i++){
+          //If the date objects are same, assuming they are duplicate entries and removing them from the array
+          if( moment.utc(scansArray[i].date).diff( moment.utc(scansArray[i+1].date) ) == 0 ){
+            scansArray.splice(i,1);
+          }
+        }
+      })();
+      console.log("Sorted, duplicate free array: ",scansArray);
 
+      return(
+        <div id="orderStatus">
+          <div id="orderDetails">
+            <pre>
+              Shipment Details:
+              <br/>
+              <br/>
+              From: {this.state.orderJSON["result"][`${this.props.orderId}`]["shipment_detail"]["pickup_name"]}
+              <br/>
+              {this.state.orderJSON["result"][`${this.props.orderId}`]["shipment_detail"]["pickup_address"]}
+              <br/>
+              <br/>
+              To: {this.state.orderJSON["result"][`${this.props.orderId}`]["shipment_detail"]["drop_name"]}
+              <br/>
+              {this.state.orderJSON["result"][`${this.props.orderId}`]["shipment_detail"]["drop_address"]}
+              <br/>
+              <br/>
+              Box: {this.state.orderJSON["result"][`${this.props.orderId}`]["shipment_detail"]["length"]}x{this.state.orderJSON["result"][`${this.props.orderId}`]["shipment_detail"]["breadth"]}x{this.state.orderJSON["result"][`${this.props.orderId}`]["shipment_detail"]["height"]} cm               {this.state.orderJSON["result"][`${this.props.orderId}`]["shipment_detail"]["weight"]} gm
+              <br/>
+              <br/>
+              Courier Partner: {this.state.orderJSON["result"][`${this.props.orderId}`]["shipment_detail"]["courier_partner"]}
+            </pre>
+          </div>
+          <div id="trackBar">
+            <ol id="trackBarApproval">
+
+            </ol>
+
+            <ol id="trackBarTransition">
+
+            </ol>
+
+            <ol id="trackBarDelivery">
+
+            </ol>
+          </div>
         </div>
-        <div id="trackBar">
-          <ol id="trackBarApproval">
+      );
+    }
+    else{
+      return(
+        <div id="orderStatus">Enter valid orderId</div>
+      );
+    }
 
-          </ol>
-
-          <ol id="trackBarTransition">
-
-          </ol>
-
-          <ol id="trackBarDelivery">
-
-          </ol>
-        </div>
-      </div>
-    );
   }
 }
 
